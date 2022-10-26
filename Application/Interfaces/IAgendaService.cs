7c8abdd8a9e2cb4a390c23f478c8018bd55cc2d6
Tiago Moura <tiagolopesdemoura@gmail.com>
2022-10-25 22:01:32 -0300

    adicionar interfaces

---
+++ Application/Interfaces/IAgendaService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IAgendaService
+    {
+        Task<PaginationResult<ContatoResponse>> BuscarContatos(ContatoParametroBusca viewModel);
+        Task<IEnumerable<ContatoTelefoneResponse>> BuscarTelefones(ContatoParametroBusca viewModel);
+        IEnumerable<TipoContatoTelefoneResponse> TiposContatoTelefone();
+        Task<ContatoResponse> ObterContato(int id);
+        Task<ContatoResponse> AdicionarContato(ContatoRequest contatoViewModel);
+        Task<ContatoResponse> AtualizarContato(int id, ContatoRequest contatoViewModel);
+        Task RemoverContato(int id);
+    }
+}
---
+++ Application/Interfaces/ILoginService.cs
@@ -0,0 +1,14 @@
+﻿using Application.ViewModels.Login;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface ILoginService
+    {
+        Task<LoginResponse> Authenticate(LoginRequest model);
+    }
+}
