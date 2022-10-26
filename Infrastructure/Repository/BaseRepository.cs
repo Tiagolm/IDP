3e9b6eccafd92caa08f7cccdac22bd37dff6e86d
Tiago Moura <tiagolopesdemoura@gmail.com>
2022-10-25 22:02:13 -0300

    add repos

---
+++ Infrastructure/Repository/BaseRepository.cs
@@ -0,0 +1,69 @@
+﻿using Domain.Core;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository
+{
+    internal class BaseRepository<T> : IBaseRepository<T> where T : Entity
+    {
+        private readonly DbSet<T> _set;
+        public IQueryable<T> Query { get; private set; }
+
+        protected BaseRepository(ApplicationContext applicationContext)
+        {
+            _set = applicationContext.Set<T>();
+
+            if (_set == null)
+                throw new InvalidOperationException($"Tipo inválido de DbSet: {typeof(T).Name}");
+
+            Query = _set.AsQueryable();
+        }
+
+        public async Task Add(T obj)
+        {
+            await _set.AddAsync(obj);
+        }
+        public Task Update(T obj)
+        {
+            _set.Update(obj);
+            return Task.CompletedTask;
+        }
+
+        public async Task Delete(int id)
+        {
+            var obj = await _set.FirstOrDefaultAsync(x => x.Id == id);
+
+
+            if (obj == null)
+                throw new InvalidOperationException($"Id: {id} para remover {typeof(T).Name} é invalido");
+
+            await Remove(obj);
+        }
+
+        public async Task<IEnumerable<T>> List()
+        {
+            return await Query.ToListAsync();
+        }
+
+        public Task<T> GetById(int id)
+        {
+            return Query.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public void AddPreQuery(Func<IQueryable<T>, IQueryable<T>> query)
+        {
+            Query = query(Query);
+        }
+
+        private Task Remove(T obj)
+        {
+            _set.Remove(obj);
+            return Task.CompletedTask;
+        }
+    }
+}
