392c242e35d66c26d561e1f13de4bba942c991dd
Tiago Moura <tiagolopesdemoura@gmail.com>
2022-10-27 22:58:17 -0300

    add automapper

---
+++ Application/Mappers/ModelToResponseProfile.cs
@@ -0,0 +1,33 @@
+﻿using Application.ViewModels.Contact;
+using Application.ViewModels.User;
+using Application.ViewModels.UserRole;
+using Domain.Core;
+using Domain.Models.Auth;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Application.ViewModels.PhoneContact;
+using Application.ViewModels.PhoneContactType;
+
+namespace Application.Mappers
+{
+    public class ModelToResponseProfile : Profile
+    {
+        public ModelToResponseProfile()
+        {
+            CreateMap<User, UserResponse>();
+            CreateMap<UserRole, UserRoleResponse>();
+
+            CreateMap<Contact, ContactResponse>();
+            CreateMap<Contact, AdminContactResponse>();
+            CreateMap<PhoneContact, PhoneContactResponse>();
+            CreateMap<PhoneContactType, PhoneContactTypeResponse>();
+
+            CreateMap(typeof(PaginationResult<>), typeof(PaginationResult<>));
+        }
+    }
+}
---
+++ Application/Mappers/RequestToModelProfile.cs
@@ -0,0 +1,30 @@
+﻿using Application.ViewModels.Contact;
+using Application.ViewModels.User;
+using AutoMapper;
+using Domain.Models.Auth;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.ViewModels.PhoneContact;
+using Application.Extensions;
+
+namespace Application.Mappers
+{
+    public class RequestToModelProfile : Profile
+    {
+        public RequestToModelProfile()
+        {
+            CreateMap<ContactRequest, Contact>()
+                .MergeList(x => x.Phones, vm => vm.Phones);
+
+            CreateMap<UserRequest, User>();
+
+            CreateMap<PhoneContactRequest, PhoneContact>()
+                .ForMember(x => x.Ddd, m => m.MapFrom(vm => vm.FormattedPhone.SplitFormattedPhone().Item1))
+                .ForMember(x => x.Phone, m => m.MapFrom(vm => vm.FormattedPhone.SplitFormattedPhone().Item2));
+        }
+    }
+}
