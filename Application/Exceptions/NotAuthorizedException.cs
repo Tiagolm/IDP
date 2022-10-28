8464f55e76d7a4c1b8dc934ac81610c8ba3eb900
Tiago Moura <tiagolopesdemoura@gmail.com>
2022-10-27 22:58:05 -0300

    add exception

---
+++ Application/Exceptions/NotAuthorizedException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Exceptions
+{
+    public class NotAuthorizedException : Exception
+    {
+        public NotAuthorizedException() : base("Usuário logado não autorizado.")
+        {
+        }
+    }
+}
