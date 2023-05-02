50e4a54654a450c90282dac754355bffb7eecfe9
Tiago Moura <tiagolopesdemoura@gmail.com>
2023-05-01 23:48:25 -0300

    adicionar automapper profiles

---
+++ Tests/Application/AutoMapperFixture.cs
@@ -0,0 +1,29 @@
+﻿using Application.Mappers;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Application
+{
+    public class AutoMapperFixture : IDisposable
+    {
+        public IMapper GetMapper()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new RequestToModelProfile());
+                cfg.AddProfile(new ModelToResponseProfile());
+            });
+
+            return config.CreateMapper();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
