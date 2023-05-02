05c6732a87b09f3039f59458de9ba20c53d541ca
Tiago Moura <tiagolopesdemoura@gmail.com>
2023-05-01 23:50:57 -0300

    adicionar repository tests

---
+++ Tests/Infra.Data/Repositories/BaseRepositoryTest.cs
@@ -0,0 +1,129 @@
+﻿using Castle.Core.Resource;
+using Domain.Interfaces;
+using Domain.Models;
+using Infrastructure;
+using Infrastructure.Data;
+using Infrastructure.Repository;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tests.Fakers.Models;
+using Xunit;
+
+namespace Tests.Infra.Data.Repositories
+{
+    [Trait("TestType", "Unit")]
+    public class BaseRepositoryTest : IClassFixture<DbTest>
+    {
+        private BaseRepository<Contact> _baseRepository;
+        private ContactRepository _contactRepository;
+        private readonly UnitOfWork _unitOfWork;
+
+        public BaseRepositoryTest(DbTest dbTest)
+        {
+            var serviceProvider = dbTest.ServiceProvider;
+            var context = serviceProvider.GetRequiredService<ApplicationContext>();
+
+            _baseRepository = new BaseRepository<Contact>(context);
+            _contactRepository = new ContactRepository(context);
+            _unitOfWork = new UnitOfWork(context);
+        }
+
+        [Fact(DisplayName = "GetByIdAsync - Registra e valida se ao consultar por Id retorna o mesmo Contato registrado anteriormente", Skip = "Tests need database. Run only on localhost.")]
+        public async Task GetByIdAsync_ContatoRegistrado_DeveRetornarMesmoContato()
+        {
+            //Arrange
+            var contact = ContactFaker.ContactGenerate();
+            var phones = ContactFaker.PhonesGenerate();
+
+            contact.Phones = phones;
+
+            var addResult = await _contactRepository.Add(contact);
+            await _unitOfWork.Save();
+
+            //Act
+            var result = await _baseRepository.GetById(addResult.Id);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.True(result.Id != 0);
+            Assert.Equal(addResult.Id, result.Id);
+            Assert.Equal(addResult.Name, result.Name);
+        }
+
+        [Fact(DisplayName = "GetByIdAsync - Valida se retorna nulo ao consultar Contato por ID inválido", Skip = "Tests need database. Run only on localhost.")]
+        public async Task GetByIdAsync_IdInvalidoDeContato_DeveRetornarNulo()
+        {
+            var result = await _baseRepository.GetById(9999);
+
+            Assert.Null(result);
+        }
+
+        [Fact(DisplayName = "GetOneAsync - Registra e valida se ao consultar por telefones retorna o mesmo Contato registrado anteriormente", Skip = "Tests need database. Run only on localhost.")]
+        public async Task GetOneAsync_ContatoRegistrado_DeveRetornarMesmoContato()
+        {
+            //Arrange
+            var contact = ContactFaker.ContactGenerate();
+            var phones = ContactFaker.PhonesGenerate();
+
+            contact.Phones = phones;
+
+            var addResult = await _contactRepository.Add(contact);
+            await _unitOfWork.Save();
+
+            //Act
+            _baseRepository.AddPreQuery(x => x.Where(c => c.Phones == addResult.Phones));
+            var result = (await _contactRepository.List()).FirstOrDefault();
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.True(result.Id != 0);
+            Assert.Equal(addResult.Id, result.Id);
+            Assert.Equal(addResult.Phones, result.Phones);
+        }
+
+        [Fact(DisplayName = "GetOneAsync - Valida se retorna nulo ao consulta Contato por nome", Skip = "Tests need database. Run only on localhost.")]
+        public async Task GetOneAsync_ContatoNaoRegistrado_DeveRetornarNull()
+        {
+            _baseRepository.AddPreQuery(x => x.Where(c => c.Name.Equals("teste")));
+            var result = (await _baseRepository.List()).FirstOrDefault();
+
+            Assert.Null(result);
+        }
+
+        [Fact(DisplayName = "GetByIdAsync - Registra e valida se ao consultar por nome retorna o mesmo Contato registrado anteriormente", Skip = "Tests need database. Run only on localhost.")]
+        public async Task ExistsAsync_ContatoRegistrado_DeveRetornarTrue()
+        {
+            //Arrange
+            var contact = ContactFaker.ContactGenerate();
+            var phones = ContactFaker.PhonesGenerate();
+
+            contact.Phones = phones;
+
+            var addResult = await _contactRepository.Add(contact);
+            await _unitOfWork.Save();
+
+            //Act
+            _baseRepository.AddPreQuery(x => x.Where(c => c.Name == addResult.Name));
+            var result = (await _contactRepository.List()).FirstOrDefault();
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.True(result.Id != 0);
+            Assert.Equal(addResult.Id, result.Id);
+            Assert.Equal(addResult.Name, result.Name);
+        }
+
+        [Fact(DisplayName = "ExistsAsync - Testa não existência de Contato", Skip = "Tests need database. Run only on localhost.")]
+        public async Task ExistsAsync_ContatoNaoRegistrado_DeveRetornarFalse()
+        {
+            _baseRepository.AddPreQuery(x => x.Where(c => c.Name.Equals("teste")));
+            var result = (await _contactRepository.List()).Any();
+
+            Assert.False(result);
+        }
+    }
+}
---
+++ Tests/Infra.Data/Repositories/ContactRepositoryTest.cs
@@ -0,0 +1,102 @@
+﻿using Castle.Core.Resource;
+using Domain.Models;
+using Infrastructure;
+using Infrastructure.Data;
+using Infrastructure.Repository;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tests.Fakers.Models;
+using Xunit;
+
+namespace Tests.Infra.Data.Repositories
+{
+    [Trait("TestType", "Unit")]
+    public class ContatoRepositoryTest : IClassFixture<DbTest>
+    {
+        private ContactRepository _contactRepository;
+        private readonly UnitOfWork _unitOfWork;
+
+        public ContatoRepositoryTest(DbTest dbTest)
+        {
+            var serviceProvider = dbTest.ServiceProvider;
+            var context = serviceProvider.GetRequiredService<ApplicationContext>();
+
+            _contactRepository = new ContactRepository(context);
+            _unitOfWork = new UnitOfWork(context);
+        }
+
+        [Fact(DisplayName = "AddContato", Skip = "Tests need database. Run only on localhost.")]
+        public async Task AddAsync_ValidContato_Successful()
+        {
+            //Arrange
+            var contact = ContactFaker.ContactGenerate();
+            var phones = ContactFaker.PhonesGenerate();
+
+            contact.Phones = phones;
+
+            //Act
+            var addResult = await _contactRepository.Add(contact);
+            await _unitOfWork.Save();
+
+            //Assert
+            Assert.NotNull(addResult);
+            Assert.True(addResult.Id != 0);
+            Assert.True(addResult.Phones is not null);
+            Assert.Equal(contact.Name, addResult.Name);
+        }
+
+        [Fact(DisplayName = "UpdateContato - Registra e atualiza Contato com sucesso", Skip = "Tests need database. Run only on localhost.")]
+        public async Task UpdateContato_ValidContato_Successful()
+        {
+            //Arrange
+            var contact = ContactFaker.ContactGenerate();
+            var phones = ContactFaker.PhonesGenerate();
+
+            contact.Phones = phones;
+
+            contact = await _contactRepository.Add(contact);
+            await _unitOfWork.Save();
+
+            var newContato = ContactFaker.ContactGenerate();
+
+            //Act
+            contact.Name = newContato.Name;
+            contact.Phones = newContato.Phones;
+           
+            await _contactRepository.Update(contact);
+            await _unitOfWork.Save();
+
+            //Assert
+            Assert.NotNull(contact);
+            Assert.True(contact.Id != 0);
+            Assert.Equal(contact.Name, newContato.Name);
+            Assert.Equal(contact.Phones, newContato.Phones);
+        }
+
+        [Fact(DisplayName = "RemoveContato - Valida remoção de Contato existente", Skip = "Tests need database. Run only on localhost.")]
+        public async Task RemoveContato_ValidContato_Successful()
+        {
+            //Arrange
+            var contact = ContactFaker.ContactGenerate();
+            var phones = ContactFaker.PhonesGenerate();
+
+            contact.Phones = phones;
+
+            var addResult = await _contactRepository.Add(contact);
+            await _unitOfWork.Save();
+
+            //Act
+            await _contactRepository.Delete(addResult.Id);
+            await _unitOfWork.Save();
+
+            var result = await _contactRepository.GetById(contact.Id);
+
+            //Assert
+            Assert.Null(result);
+        }
+    }
+}
